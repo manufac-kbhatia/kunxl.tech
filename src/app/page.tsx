2f828a14c74c4ca1cbb681420dcf14386fdacaf5
Kunalkbhatia <bhatiakbkb@gmail.com>
2024-12-12 20:09:16 +0530

    Framer motion added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ export default function Home() {
   return (
     <>
         <div className="flex flex-col items-center justify-center text-white">
+        <div className="custom-gradient"></div>
           <h1 className="text-4xl font-bold h-screen">Hello</h1>
           <p className="text-lg mt-4 h-screen">This is the content over the noisy background.</p>
         </div>
