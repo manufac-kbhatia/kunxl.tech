69404d3d319ed797380a72ed68c449732458ed9b
kunalkbhatia <bhatiakbkb@gmail.com>
2025-01-19 13:22:39 +0530

    skill tile improved

--- src/components/Tile/index.tsx
+++ src/components/Tile/index.tsx
@@ -4,8 +4,10 @@ import { Skill } from "@/utils/utils";
 export function Tile({ skill }: { skill: Skill }) {
   return (
     <div
-      className="flex w-[160px] items-center justify-center gap-4 rounded-3xl border-[1px] border-b-8 p-3 text-xl md:w-[200px]"
-      style={{ borderColor: skill.color }}
+      className={`flex w-[160px] items-center justify-center gap-4 rounded-[5px] border-solid border-[1px] border-gray-700 p-3 text-md md:w-[200px]`}
+      style={{ 
+        boxShadow: `6px 6px 1px ${skill.color}`,
+       }}
     >
       <Image src={skill.image} alt="javascript" width={30} height={30} />
       <div>{skill.skill}</div>
