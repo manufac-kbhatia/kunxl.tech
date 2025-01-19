8ad4bef3f970dc138a73e56b7a63ef5cb1ad854d
kunalkbhatia <bhatiakbkb@gmail.com>
2025-01-19 13:26:58 +0530

    header improved

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,7 +5,13 @@ export function Header() {
     <div
       className={`fixed inset-0 z-30 p-5`}
     >
-      <nav className="flex justify-between items-center gap-5 p-4 rounded-lg max-w-2xl mx-auto text-2xl backdrop-blur-lg text-customBlue">
+       <Link
+          href="https://drive.google.com/drive/folders/1LlIYE-RSdiewmPfpWViG3Cd2I6DWeAZW?usp=sharing"
+          className="md:hidden rounded-full bg-customYellow p-2 text-2xl text-customShade"
+        >
+          CV
+        </Link>
+      <nav className="hidden md:flex justify-between items-center gap-5 p-4 rounded-lg max-w-2xl mx-auto text-2xl backdrop-blur-lg bg-white/10 text-customBlue">
         <Link href="#about">About</Link>
         <Link href="#skills">Skills</Link>
         <Link href="#work">Work</Link>
